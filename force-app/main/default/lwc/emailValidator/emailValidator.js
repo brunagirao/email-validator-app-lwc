fb766de875c9f94b3efd53a8e618147bf4a234f9
Bruna Girão <brunagirao.s@gmail.com>
2023-05-28 09:36:08 -0300

    1st version emailValidator lwc

--- force-app/main/default/lwc/emailValidator/emailValidator.js
+++ force-app/main/default/lwc/emailValidator/emailValidator.js
@@ -1,8 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track }  from 'lwc';
 
+import validateEmail                from '@salesforce/apex/Ws_EmailValidatorIntegration.validateEmail';
 
 export default class EmailValidator extends LightningElement {
 
+    @track emailToValidate;
 
 
+    //HANDLES
+    handleEmailChange (event) {
+        this.emailToValidate = event.target.value;
+    }
+
+    handleValidateEmail () {
+        console.log('handleValidateEmail');
+
+        if(this.isAValidEmail(this.emailToValidate)) this.validateEmail();
+    }
+
+    //ASYNCS
+    async validateEmail () {
+        console.log('validateEmail');
+
+        let response = await validateEmail ({
+            emailToValidate : this.emailToValidate
+        });
+    
+        let emailResponse = JSON.parse(response.ResponseJSON);
+
+        if (response.HasError || emailResponse.length < 1) {
+            console.log('Error');
+        } else {
+            console.log('Success', response.ResponseJSON);
+        }
+
+    }
+
+
+    isAValidEmail(email) {
+        console.log('email => ', email);
+        return String(email)
+        .toLowerCase()
+        .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    }
 }
